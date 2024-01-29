a5a2cdd3d21def524a07d58abbdc104a15ccbf38
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-01-29 10:06:30 +0200

    refactoring and stuff. Trynna figure out the some weird space filling stuff

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ function val_Password() {
     if (password != conf_password && conf_password!=null) {
         document.getElementById('wrong-password-msg').style.color = 'red';
         document.getElementById('wrong-password-msg').innerHTML = "Passwords don't match";
+
+        document.getElementById("confirm_password").style.border = '3px rgb(151,37,37) solid;';
     }
     else{
         document.getElementById('wrong-password-msg').innerHTML ="";
+        document.getElementById("confirm_password").style.border = "border: 2px rgb(38, 151, 38) solid;"
     }
 }
